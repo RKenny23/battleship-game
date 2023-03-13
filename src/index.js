ab4b3a8e1c0b96533b51eaf40fac3786df634ea9
RKenny23 <rkenny238@hotmail.com>
2023-03-13 13:59:51 -0400

    added buttons

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 function createGrid(gridId) {
   const grid = document.getElementById(gridId);
   for (let i = 0; i < 10; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
+    const row = document.createElement('div');
+    row.className = 'row';
     for (let j = 0; j < 10; j++) {
-      const square = document.createElement("div");
-      square.className = "square";
+      const square = document.createElement('div');
+      square.className = 'square';
       row.appendChild(square);
     }
     grid.appendChild(row);
   }
 }
 
-createGrid("player-1-grid");
-createGrid("player-2-grid");
+createGrid('player-1-grid');
+createGrid('player-2-grid');
 
+const shipContainer = document.querySelector('.ship-container');
+const flipButton = document.querySelector('#flip-button');
 
+function flip() {
+  const optionShips = Array.from(shipContainer.children);
+
+  optionShips.forEach(
+    (optionShip) =>
+      (optionShip.getElementsByClassName.transform = `rotate(90deg)`)
+  );
+
+  optionShips.forEach(
+    (optionShip) => (optionShip.getElementsByClassName.transform = 'scale(2)')
+  );
+
+  console.log(Array.from(shipContainer.children));
+}
+
+flipButton.addEventListener('click', flip);
